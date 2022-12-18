a843630f707db5f60e4ae45fa32ba22dc5dde4f9
mostafa mohamed abobakr <116291044+mostafa-abobakr@users.noreply.github.com>
2022-12-18 15:41:45 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,20 +1,40 @@
-﻿using System.Runtime.CompilerServices;
-Console.Write("Enter the first number: ");
-int n1 = int.Parse(Console.ReadLine());
-Console.Write("Enter the second number: ");
-int n2 = int.Parse(Console.ReadLine());
-Console.Write("\nprime numbers from " + n1 + " to " + n2 + " are: ");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-for (int i = n1; i <= n2; i++)
+namespace Perfectnumbers
 {
-    int j;
-    for (j = 2; j < i; j++)
+    class Program
     {
-        if (i % j == 0)
-            break;
+        static bool PerfectNum(int number)
+        {
+            int sum = 0;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0) sum = sum + i;
+            }
+            if (sum == number)
+            { return true; };
+            return false;
+        }
 
+        static void Main(string[] args)
+        {
+            int n1, n2;
+            Console.Write("Enter the first number: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            Console.Write("\nPerfect numbers from " + n1 + " to " + n2 + " are: ");
+            for (int i = n1; i <= n2; i++)
+            {
+                if (PerfectNum(i)) Console.Write("{0} , ", i);
+            }
+
+            Console.WriteLine("\n");
+        }
     }
-    if (i == j)
-        Console.Write(i + " , ");
 }
-Console.WriteLine("\n");
