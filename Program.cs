96c5bc278026345bdae44fea6d22771e6cf4d7f6
mostafa mohamed abobakr <116291044+mostafa-abobakr@users.noreply.github.com>
2022-12-18 15:40:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+Console.Write("Enter the first number: ");
+int n1 = int.Parse(Console.ReadLine());
+Console.Write("Enter the second number: ");
+int n2 = int.Parse(Console.ReadLine());
+Console.Write("\nprime numbers from " + n1 + " to " + n2 + " are: ");
+
+for (int i = n1; i <= n2; i++)
+{
+    int j;
+    for (j = 2; j < i; j++)
+    {
+        if (i % j == 0)
+            break;
+
+    }
+    if (i == j)
+        Console.Write(i + " , ");
+}
+Console.WriteLine("\n");
